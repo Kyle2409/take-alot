9cb60080709a32ab3dbbd0fcf316adcc08933bf6
Kyle Mc Bryne <kylemcb.24@gmail.com>
2021-10-19 12:24:07 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+
+window[window["TiktokAnalyticsObject"]].instance("C4S6HV96H18A0MH1A6U0").setAdvancedMatchingAvailableProperties({"email":false,"phone_number":false});
+window[window["TiktokAnalyticsObject"]].instance("C4S6HV96H18A0MH1A6U0").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C4S6HV96H18A0MH1A6U0").setPixelInfo({status: 0, name: "TikTok Pixel", advertiserID: "6964303189428453378", setupMode: 2, partner: "", is_onsite: false});
+window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("takealot.com");
+window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig([]);
